5ab7f41b796a09f0a42e28fcce95e89686958ac1
infinitebattery7 <shriyash1111@gmail.com>
2014-01-24 17:47:43 -0500

    update

--- web.js
+++ web.js
@@ -80,6 +80,9 @@ app.post('/query', function(req, res) {
 var server = http.createServer(app);
 
 var port = process.env.PORT || 5000;
+
+server.listen(process.env.PORT || 5000);
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
